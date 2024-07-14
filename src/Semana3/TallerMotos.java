b09a600b41a19e3aeec79de94d9926a91a5a294b
Nar Mena <narciris.tech@gmail.com>
2024-07-14 16:20:40 -0500

    ajustes

--- src/Semana3/TallerMotos.java
+++ src/Semana3/TallerMotos.java
@@ -1,12 +1,13 @@
 package Semana3;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class TallerMotos {
 
 
 
-
+// principal para ejecutar las funcionalidades y correr el menu
     public static void main(String[] args) {
         Scanner dataEntry = new Scanner(System.in);
         int maxEmployees;
@@ -50,12 +51,20 @@ public class TallerMotos {
                     System.out.println("escriba marca del vehiculo");
                     dataEntry.nextLine();
                     String brandOrModel = dataEntry.nextLine();
-                    showVehicleByModel(maxEmployees, brandOrModel,maxVhicles);
+                    showVehicleByModel(brandOrModel);
                     break;
                 case 4:
                     countVehicles(maxVhicles, maxEmployees);
                     break;
-                case 5:break;
+                case 5:
+                    String brandModel2;
+                    System.out.println("ingresa la marca o modelo");
+                    dataEntry.nextLine();
+                    brandModel2 = dataEntry.nextLine();
+
+                    updateVehicle(brandModel2,dataEntry);
+                    break;
+
                 case 6:
                     System.out.println("saliendo del sistema");
                     break;
@@ -76,13 +85,17 @@ public class TallerMotos {
     }
     // declarar array tridimensional
     public static String[][][] mechanic;
+    // inicializar la matrix tridimencional de manera global
 
+
+    // metodo para registrar un trabajo
     public static void registerEmployee(Scanner dataEntry, int max, int maxVehicles){
-        int typeIndex = 0;
+        int typeIndex = 0;  // variable para validar el tipo de vehiculo que ingresara el usario de la app
         String brand;
         String modeloOfcar;
         String year;
         String status;
+        // recorrer teniendo en cuenta el maximo ingresado por el user
        for(int i=0;i<max; i++){
            System.out.println("ingrese el nombre del empleado" + (i+1));
            dataEntry.nextLine();
@@ -131,16 +144,19 @@ public class TallerMotos {
            }
            }
     }
+// metodo para imprimir datos
 
     public static void imprimirDatos(int maxEmployees, int maxVehicles) {
         System.out.println("Agenda de trabajos");
         System.out.println("");
         System.out.println("|Tipo|Marca|Modelo|Año|Estado|");
+        // recorrer la matrix usando el tamaño ingrsado por user
         for (int i = 0; i < maxEmployees; i++) {
 
                 for (int j = 0; j < maxVehicles; j++) {
-                    if (mechanic[i][j][0] != null) {
-                        String tipo = (j == 0) ? "Moto" : "Carro";
+                    if (mechanic[i][j][0] != null) { // usar estos indices generados para verificar si el elemento esta iniciado en null
+                        // osea no tiene datos
+                        String tipo = (j == 0) ? "Moto" : "Carro"; // imprimir segun el tipo de vehiculo
                         System.out.println("|" + tipo + "|" + mechanic[i][j][0] + "|" + mechanic[i][j][1] + "|" + mechanic[i][j][2] + "|" + mechanic[i][j][3] + "|");
                     }
 
@@ -148,34 +164,32 @@ public class TallerMotos {
         }
     }
 
-    public static void showVehicleByModel(int maxEmployees,String brandOrModel, int maxVehicle){
-        boolean found = false;
-        System.out.println("|Tipo|Marca|Modelo|Año|Estado|");
-        for(int i =0; i< maxEmployees; i++){
-            for(int j=0; j<maxVehicle; j++){
-                if(mechanic[i][j][0] != null
-                        && (mechanic[i][j][0].equals(brandOrModel)
-                        || mechanic[i][j][1].equals(brandOrModel))){
+    // mostrar todos los vehiculos guardados
+    // usando otro metodo que se encarga de buscar
+    public static void showVehicleByModel(String brandOrModel){
+         int[] index = findVehicle(brandOrModel); // guardar el array generado
+         if(index != null){ // validar que el metodo index sea diferente de nukk
+             // es decir que haya elemento encontrados
+             System.out.println("|Tipo|Marca|Modelo|Año|Estado|");
+             int firstSize = index[0];
+             int secondSize = index[1];
+             String type = (secondSize == 0 ) ? "Moto" : "Carro";
+             System.out.println(type + mechanic[firstSize][secondSize][0] + "|" + mechanic[firstSize][secondSize][1] + "|" + mechanic[firstSize][secondSize][2] + "|" + mechanic[firstSize][secondSize][3] + "|");
 
-                        String tipo = (j == 0) ? "Moto" : "Carro";
-                        System.out.println("|" + tipo + "|" + mechanic[i][j][0] + "|" + mechanic[i][j][1] + "|" + mechanic[i][j][2] + "|" + mechanic[i][j][3] + "|");
-                        found = true;
-
-                }
-            }
-
-            } if(!found){
-            System.out.println("no encontrado");
+         } else {
+             System.out.println("no encontrado"); // si es null, es porque no ha encontrado una coincidencia
+         }
         }
-        }
-
+// metodo para contar vehiculos por estado
         public static void countVehicles(int maxEmployee, int maxVehicle){
+        // inicializar contadores
             int vehiclePendind = 0;
             int repairedVehicle =0;
             int inRepair = 0;
-         for(int i=0; i< maxEmployee; i++){
+         for(int i=0; i< maxEmployee; i++){ // recorrer el array tridimensional por dimensiones
              for(int j=0; j<maxVehicle;j++){
                  if(mechanic[i][j][0] != null){
+                     // el metod equals ignore case, devuelve una cada de caracteres igual en longitud y caracteres, sin importar mayus o minus
                      if(mechanic[i][j][3].equalsIgnoreCase("pendiente")){
                        vehiclePendind ++;
                      } else if(mechanic[i][j][3].equalsIgnoreCase("reparado")){
@@ -189,18 +203,45 @@ public class TallerMotos {
          }
         }
 
-        public static int[] findVehicle(int maxEmployees, int maxVehicle){
-            for (int i =0; i<maxEmployees; i++){
-                for(int j=0; j<maxVehicle; j++){
-                    if(mechanic[i][j][0] != null){
-                        return new int[] {i,j};
+        // methodo para buscar un vehicle usando marca o modelo
+    // el metodo recorre el array tridimensional
+    // validando si hay un indice que corresponda a la la palabra ingresada por el user
+    // devuelve el indice de los elementos encontrados
+    // dimension 1 y dimension 2
+        public static int[] findVehicle(String brandOrModel){
+            for(int i=0; i<mechanic.length; i++){
+                for(int j=0; j<mechanic[i].length; j++){
+                    if(mechanic[i][j][0] != null
+                            && (mechanic[i][j][0].equalsIgnoreCase(brandOrModel)
+                            || mechanic[i][j][1].equalsIgnoreCase(brandOrModel))){
+                        return  new int[] {i,j};
                     }
                 }
-
+                }
+            return null;
             }
-            return  null;
-        }
+
+
+            // metodo para actualizar el estado del vehiculo
+    // usa el metodo findvhicle
+    // con el index que devuelve
+    // se actualiza el nuevo estado del elemento que coincide
+    public static void updateVehicle(String brandOrModel,Scanner dataEntry){
+         int[] index = findVehicle(brandOrModel); // guarda el elemto retornado
+         if(index != null){ // verifica si hay una coincidencia
+             System.out.println("ingresa el nuevo estado"); // pide el dato
+             String newStatus = dataEntry.nextLine();
+             int firstSize = index[0]; // guarda indice
+             int secondSize = index[1];
+             mechanic[firstSize][secondSize][3] = newStatus;
+             System.out.println("actualizado");
+         } else{
+             System.out.println("no encontrado");
+         }
     }
+
+}
+
 
 
 
